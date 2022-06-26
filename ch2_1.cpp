5d9150aeada2dc324838f094c298c6d28979c818
xia <1107239867@qq.com>
2022-06-26 09:26:51 +0800

    Add files via upload

---
+++ ch2_1.cpp
@@ -0,0 +1,18 @@
+//ch2_1.cpp
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	double amount= 22.0/7;
+	cout<<amount<<endl;
+	cout<<setprecision(0)<<amount<<endl
+		<<setprecision(1)<<amount<<endl
+		<<setprecision(2)<<amount<<endl
+		<<setprecision(3)<<amount<<endl
+		<<setprecision(4)<<amount<<endl;
+	cout<<fixed<<setprecision(8)<<amount<<endl;
+	cout<<scientific<<amount<<endl;
+	cout<<setprecision(6);
+	return 0;
+}
---
+++ ch2_6.cpp
@@ -0,0 +1,10 @@
+//ch2_6
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout<<10<<" "<<-20<<endl
+		<<showpos<<10<<" "<<-20<<endl;
+	return 0;
+}
